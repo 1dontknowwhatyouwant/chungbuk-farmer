4692fd916231d6a44ddc8d68261905fd827d94ff
1dontknowwhatyouwant <milestone0917@gmail.com>
2026-08-28 00:29:50 +0900

    feat: 로그인 및 회원가입 컴포넌트에서 사용하지 않는 이미지 파일 제거

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,9 +1,4 @@
-import naver from "./images/login/naver.png";
-import kakao from "./images/login/kakao.png";
-import apple from "./images/login/apple.png";
 import chungbukFarmerLogo from "./images/login/chungbuk-farmer-logo.svg";
-import signupEllipse from "./images/signup/ellipse.svg";
-import signupEllipseWide from "./images/signup/ellipse-wide.svg";
 import signupEye from "./images/signup/eye.svg";
 import signupMountainLarge from "./images/signup/mountain-large.svg";
 import signupMountainSmall from "./images/signup/mountain-small.svg";
@@ -25,12 +20,7 @@ import homeAlarm from "./images/home/alarm.png";
 import homeLogo from "./images/home/chungbuk-farmer-logo.svg";
 
 export {
-  naver,
-  kakao,
-  apple,
   chungbukFarmerLogo,
-  signupEllipse,
-  signupEllipseWide,
   signupEye,
   signupMountainLarge,
   signupMountainSmall,
